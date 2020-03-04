6589599b19904a4a8df77286135a45c2a8758941
JCBoyd <jcboyd@pm.me>
2020-03-03 17:16:12 -0700

    Added Class method display() to filter and display results

--- src/boxclassproject/Box.java
+++ src/boxclassproject/Box.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package boxclassproject;
 
 /******************************************************************************
@@ -30,7 +25,6 @@ public class Box
         this(length, width, 0);
     } //end Box - 2 parameters
   
-    
     public Box()
     {
         this(1, 1, 1);
@@ -63,16 +57,35 @@ public class Box
     
     public boolean isSquare()
     {
-        return (this.length == this.width);
+        return ((this.length == this.width) && (this.height == 0));
     } //end isSquare
     
     public void display()
     {
-        
+        if (this.isCube()) //test for cube
+        {
+            System.out.printf("This box is a cube: %d on all three sides\nVolume: %d\n",
+                    this.length, this.calcVolume(this.length, this.width, this.height));
+        }
+        else if (this.isBox()) //test for container
+        {
+            System.out.printf("This box is: %d x %d x %d\nVolume: %d\n",
+                    this.length, this.width, this.height,
+                    this.calcVolume(this.length, this.width, this.height));
+        }
+        else if (this.isSquare()) //test for square
+        {
+            System.out.printf("This is a flat square: %d x %d\nPerimeter: %d\nArea: %d\n",
+                    this.length, this.width, this.calcPerimeter(this.length, this.width),
+                    this.calcArea(this.length, this.width));
+        }
+        else //all others - must be rectangle
+        {
+            System.out.printf("This is a rectangle: %d x %d\nPerimeter: %d\nArea: %d\n",
+                    this.length, this.width, this.calcPerimeter(this.length, this.width),
+                    this.calcArea(this.length, this.width));
+        } //end if
+            
     } //end display
-    
-    
-    
-    
-    
+     
 } //end class Box
